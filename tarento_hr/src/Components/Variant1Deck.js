8e32b28d6e0477c70e93f8ec9609a7e968dbdf9b
Arathi <arathivinodwrk@gmail.com>
2024-01-08 17:40:09 +0530

    style changes in deck1

--- tarento_hr/src/Components/Variant1Deck.js
+++ tarento_hr/src/Components/Variant1Deck.js
@@ -1,9 +1,13 @@
 import React from "react";
 import axios from "axios";
-import { Container, Row, Col, Card,Image } from "react-bootstrap";
+import { Container, Row, Col, Card, Image } from "react-bootstrap";
 import { useEffect, useState } from "react";
 
 const Variant1Deck = ({ newJoinee }) => {
+  const customCardStyle = {
+    backgroundColor:'#d6cade', // Custom background color
+    color: '#212529', // Custom text color
+  };
   const [src, setSrc] = useState("");
   useEffect(() => {
     axios
@@ -26,34 +30,54 @@ const Variant1Deck = ({ newJoinee }) => {
 
   return (
     <Container className="resume-container1">
-      <h1 className="p-4" style={{ color: 'blue' }}>
+      <h1 className="p-4" style={{ color: "blue" }}>
         <center>WELCOME TO TARENTO </center>
       </h1>
       <Row>
+       
         {/* Left Column */}
-        <Col md={4} sm={6} xs={12}>
-          {/* Profile Picture, Name, and Role Card */}
-          <Card className="small-container mt-3">
+        <Col md={6}>
+          {/* Center Card - Photo, Name, Designation, IBU, Skills */}
+          <Card className="center-card mb-3" style={customCardStyle}>
             <Card.Body>
-              <div className="d-flex flex-column justify-content-center align-items-center">
-              <Image
-                src={src}
-                alt={newJoinee.empName}
-                roundedCircle
-                width="50%"
-              />
-                <br />
-                <b className="emp-name">{newJoinee.empName}</b>
-                <br />
-                <Card.Text>
-                  {newJoinee.roleId && newJoinee.roleId.roleName}
-                </Card.Text>
-              </div>
+              <Image src={src} alt={newJoinee.empName} roundedCircle width="25%" />
+              <br />
+              <b className="emp-name">{newJoinee.empName}</b>
+              <br />
+              <Card.Text>{newJoinee.roleId && newJoinee.roleId.roleName}</Card.Text>
+              <Card.Text>
+                <b>Manager:</b> {newJoinee.ibuId && newJoinee.ibuId.manager}
+              </Card.Text>
+              <Card.Text>
+                <b>IBU:</b> {newJoinee.ibuId && newJoinee.ibuId.ibuName}
+              </Card.Text>
+              <Card.Text>
+                <b>Core Skills:</b> {newJoinee.empSkills}
+              </Card.Text>
             </Card.Body>
           </Card>
+           {/* Experience */}
+           <Card className="emp-info-card mb-3"style={customCardStyle}>
+            <Card.Body>
+              <Card.Text>
+                <b>Experience:</b> {newJoinee.empExperience} years<br />
+               </Card.Text>
+            </Card.Body>
+          </Card>
+            {/* Hometown Card */}
+            <Card className="emp-info-card mb-3"style={customCardStyle}>
+            <Card.Body>
+              <Card.Title>Hometown</Card.Title>
+              <Card.Text>{newJoinee.empAddress}</Card.Text>
+            </Card.Body>
+          </Card>
+         
+        </Col>
 
+        {/* Right Column */}
+        <Col md={6}>
           {/* Contact Info Card */}
-          <Card className="small-container mt-3">
+          <Card className="emp-info-card mb-3"style={customCardStyle}>
             <Card.Body>
               <Card.Title>Contact me through</Card.Title>
               <Card.Text>
@@ -63,57 +87,34 @@ const Variant1Deck = ({ newJoinee }) => {
             </Card.Body>
           </Card>
 
-          {/* Experience and Education Card */}
-          <Card className="small-container mt-3">
+        
+           {/* Education Card */}
+           <Card className="emp-info-card mb-3 "style={customCardStyle}>
             <Card.Body>
-              
               <Card.Text>
-                <b>Experience:</b> {newJoinee.empExperience} years<br />
                 <b>Education:</b> {newJoinee.eduId && newJoinee.eduId.eduName}
               </Card.Text>
             </Card.Body>
           </Card>
-        </Col>
 
-        {/* Right Column */}
-        <Col md={8} sm={6} xs={12} className="right-column">
-          {/* IBU, Manager, and Skills Card */}
-          <Card className="small-container mb-3">
-            <Card.Body>
-              <Card.Text>
-                <b>IBU:</b> {newJoinee.ibuId && newJoinee.ibuId.ibuName}<br />
-                <b>Manager:</b> {newJoinee.ibuId && newJoinee.ibuId.manager}<br />
-                <b>Core Skills:</b> {newJoinee.empSkills}
-              </Card.Text>
-            </Card.Body>
-          </Card>
-
-          {/* Hometown */}
-          <Card className="small-container mb-3">
-            <Card.Body>
-              <Card.Title>Hometown</Card.Title>
-              <Card.Text>{newJoinee.empAddress}</Card.Text>
-            </Card.Body>
-          </Card>
-
-          {/* Free Time Hobbies */}
-          <Card className="small-container mb-3">
+          {/* Free Time Hobbies Card */}
+          <Card className="emp-info-card"style={customCardStyle}>
             <Card.Body>
               <Card.Title>In my free time, I enjoy</Card.Title>
               <Card.Text>{newJoinee.empHobbie}</Card.Text>
             </Card.Body>
           </Card>
 
-          {/* WWIB */}
-          <Card className="small-container mb-3">
-  <Card.Body>
-    <Card.Title>If I Wasn't in my Current Profession, I would have been</Card.Title>
-    <Card.Text className="wwib-text">{newJoinee.wwib}</Card.Text>
-  </Card.Body>
-</Card>
+          {/* WWIB Card */}
+          <Card className="emp-info-card mb-3"style={customCardStyle}>
+            <Card.Body>
+              <Card.Title>If I Wasn't in my Current Profession, I would have been</Card.Title>
+              <Card.Text className="wwib-text">{newJoinee.wwib}</Card.Text>
+            </Card.Body>
+          </Card>
 
-          {/* Quote */}
-          <Card className="small-container">
+          {/* Quote Card */}
+          <Card className="emp-info-card"style={customCardStyle}>
             <Card.Body>
               <Card.Title>Quote That Fuels Me</Card.Title>
               <Card.Text>"{newJoinee.empQuote}"</Card.Text>
@@ -126,3 +127,5 @@ const Variant1Deck = ({ newJoinee }) => {
 };
 
 export default Variant1Deck;
+
+
